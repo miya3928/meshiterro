36ccabf115e0e3f34051a044c1b2b39f18b22c6c
TatsuyaMiyagawa <181440097+miya3928@users.noreply.github.com>
2024-10-15 15:09:14 +0000

    [fimish]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-root to: "homes#top"
+devise_for :users
+root to: 'homes#top'
+get "homes/about", to: "homes#about", as: "about"
+resources :post_images, only: [:new, :create, :index, :show, :destroy]
+resources :users, only: [:show, :edit]
 
-get 'homes/about', to: 'homes#about', as: 'about'
 end
